04cbbbfbaa49063d4d6c9be5037ebd00766ce786
amqicher <ayoubamqicher@gmail.com>
2026-03-23 16:11:58 +0100

    feat(auth): add login endpoint

--- app/main.py
+++ app/main.py
@@ -22,3 +22,11 @@ def health_check():
         "service": "git-workflow-demo-clean",
         "version": "0.1.0"
     }
+
+
+@app.post("/auth/login")
+def login():
+    return {
+        "message": "User login endpoint",
+        "success": True
+    }
